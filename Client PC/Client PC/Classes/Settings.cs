f44b671438e1b62a4640f4a4b9da900c2c90645d
Beriktabe <Beriktabe@gmail.com>
2025-02-14 11:40:50 +0300

    Add files via upload

---
+++ Client PC/Client PC/Classes/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beriktabe.Settings
+{
+    class Settings
+    {
+        public static string ip = "localhost";
+        public static int port = 1010;
+        public static string path = Environment.CurrentDirectory + "\\BD\\Services_db.db";
+        public static string XmlPath = Environment.CurrentDirectory + "\\Saves\\data.xml";
+        public static string choose = null;
+    }
+}
---
+++ Client PC/Client PC/Classes/sql_commands.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beriktabe.SqlWorker
+{
+    class sql_commands
+    {
+        public static SQLiteConnection connection;
+
+        public static string[] GetTables()
+        {
+            List<string> mass = new List<string>();
+
+            // listBox1.Items.Clear();
+            SQLiteCommand command = new SQLiteCommand(connection);
+            connection.Open();
+            command.CommandText = "SELECT tbl_name FROM sqlite_master";
+
+            using (SQLiteDataReader rdr = command.ExecuteReader())
+            {
+                while (rdr.Read())
+                {
+                    mass.Add(rdr[0].ToString());
+                }
+                rdr.Close();
+            }
+            command.Dispose();
+            connection.Close();
+
+            return mass.ToArray<string>();
+        }
+
+        public static bool GetAll(string name, out string[] data1, out string[] data2)
+        {
+            List<string> mass1 = new List<string>();
+            List<string> mass2 = new List<string>();
+
+            string a = "'" + name + "'";
+            SQLiteCommand command = new SQLiteCommand(connection);
+            connection.Open();
+            command.CommandText = string.Format("SELECT * FROM {0}", a);
+            using (SQLiteDataReader rdr = command.ExecuteReader())
+            {
+                while (rdr.Read())
+                {
+                    mass1.Add(rdr[0].ToString());
+                    mass2.Add(rdr[1].ToString());
+                }
+                rdr.Close();
+            }
+            command.Dispose();
+            connection.Close();
+            data1 = mass1.ToArray<string>();
+            data2 = mass2.ToArray<string>();
+            return true;
+
+        }
+
+        public static bool CreateTable(string name)
+        {
+            string a = "'" + name + "'";
+            SQLiteCommand command = new SQLiteCommand(connection);
+            connection.Open();
+            command.CommandText = string.Format("DROP TABLE IF EXISTS {0};", a);
+            command.ExecuteNonQuery();
+
+            command.Dispose();
+            connection.Close();
+
+            return true;
+        }
+        public static bool DeleteTable(string name)
+        {
+            string a = "'" + name + "'";
+            SQLiteCommand command = new SQLiteCommand(connection);
+            connection.Open();
+            command.CommandText = string.Format("DROP TABLE IF EXISTS {0};", a);
+            command.ExecuteNonQuery();
+
+            command.Dispose();
+            connection.Close();
+
+            return true;
+        }
+
+        public static bool InsertCommands(string[] commands)
+        {
+            SQLiteCommand command = new SQLiteCommand(connection);
+            connection.Open();
+            foreach (string cmd in commands)
+            { 
+                try
+                {
+                   command.CommandText = cmd;
+                   command.ExecuteNonQuery();
+                }
+                catch { return false; }
+            }
+            command.Dispose();
+            connection.Close();
+
+            return true;
+        }
+
+        public static void ConnectToFile(string path)
+        {
+            connection = new SQLiteConnection("Data source = " + path);
+        }
+
+        public static string[] Get(string name, string table)
+        {
+            string nm = "'" + name + "'";
+            string tbl = "'" + table + "'";
+            List<string> mass = new List<string>();
+            SQLiteCommand command = new SQLiteCommand(connection);
+            connection.Open();
+            command.CommandText = string.Format("SELECT {0} FROM {1}", nm, tbl);
+            using (SQLiteDataReader rdr = command.ExecuteReader())
+            {
+                while (rdr.Read())
+                {
+                    mass.Add(rdr[0].ToString());
+                }
+                rdr.Close();
+            }
+            return mass.ToArray();
+        }
+    }
+}
