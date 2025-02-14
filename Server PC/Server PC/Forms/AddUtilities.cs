5d01c44d285c3cfdac58b3d76f8daf08fc226b13
Beriktabe <Beriktabe@gmail.com>
2025-02-14 11:36:33 +0300

    Add files via upload

---
+++ Server PC/Server PC/Forms/AddUtilities.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Server_PC;
+using Classes;
+
+namespace Forms
+{
+    public partial class AddUtilities : Form
+    {
+        public static string path = Settings.path;
+        public static string srv_name;
+        public static List<string> commandsList = new List<string>();
+        static AddUtilities addUtilities;
+        public AddUtilities()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            srv_name = null;
+            commandsList.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            srv_name = null;
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            groupBox1.Text = textBox1.Text;
+            richTextBox1.Text = "       " + textBox1.Text + Environment.NewLine;
+            richTextBox2.Text = "       " + "ТИП ДАННЫХ" + Environment.NewLine;
+            srv_name = "'" + textBox1.Text + "'";
+
+            commandsList.Insert(0, string.Format("CREATE TABLE IF NOT EXISTS {0} (name TEXT, type TEXT);", srv_name));
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) == false && button1.Enabled == false)
+            {
+                richTextBox1.Text += textBox2.Text + Environment.NewLine;
+                richTextBox2.Text += comboBox1.SelectedItem.ToString() + Environment.NewLine;
+                commandsList.Add(string.Format("INSERT INTO {0}(name, type) VALUES ('{1}', '{2}');", srv_name,  textBox2.Text, comboBox1.SelectedItem));
+            }
+            else
+                MessageBox.Show("имя услуги не заполнено" + Environment.NewLine + "или" + Environment.NewLine + "не заблокировано");
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Dispose(true);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(!File.Exists(path)) SQLiteConnection.CreateFile(path);
+            bool res = sql_commands.InsertCommands(commandsList.ToArray<string>());
+            if (res == false) MessageBox.Show("ERROR");
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            refreshList();
+            this.Dispose();
+        }
+
+        public static void refreshList()
+        {
+            addUtilities = new AddUtilities();
+            addUtilities.Show();
+        }
+    }
+}
---
+++ Server PC/Server PC/Forms/ListUtilities.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using Server_PC;
+using Classes;
+
+namespace Forms
+{
+    public partial class ListUtilities : Form
+    {
+        public ListUtilities()
+        {
+            InitializeComponent();
+        }
+        private static string path = Settings.path;
+
+        private static SQLiteConnection connection = new SQLiteConnection("Data source = " + path);
+        static AddUtilities addUtilities;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+            refreshLoad();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex > -1)
+            {
+                richTextBox1.Text = "";
+                richTextBox2.Text = "";
+
+                string[] data1;
+                string[] data2;
+
+                sql_commands.GetAll(listBox1.SelectedItem.ToString(),out data1, out data2);
+
+                for(int a = 0; a != data1.Length; a++)
+                {
+                    richTextBox1.Text += data1[a] + Environment.NewLine;
+                    richTextBox2.Text += data2[a] + Environment.NewLine;
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            refreshList();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex > -1)
+            {
+                sql_commands.DeleteTable(listBox1.SelectedItem.ToString());
+            }
+            // DROP TABLE IF EXISTS table2;
+            refreshLoad();
+        }
+
+        private static void refreshList()
+        {
+            addUtilities = new AddUtilities();
+            addUtilities.Show();
+        }
+        private void refreshLoad()
+        {
+            listBox1.Items.Clear();
+            foreach(string a in sql_commands.GetTables())
+            {
+                listBox1.Items.Add(a);
+            }
+         //   listBox1.Items.Clear();
+         //   SQLiteCommand command = new SQLiteCommand(connection);
+         //   connection.Open();
+         //   command.CommandText = "SELECT tbl_name FROM sqlite_master";
+         //   using (SQLiteDataReader rdr = command.ExecuteReader())
+         //   {
+         //       while (rdr.Read())
+         //       {
+         //           //  Console.Write("{0} ", rdr[0]);
+         //           listBox1.Items.Add(rdr[0].ToString());
+         //          //treeView1.Nodes[0].Nodes.Add("111");
+         //       }
+         //       rdr.Close();
+         //   }
+         //   command.Dispose();
+         //   connection.Close();
+        }
+    }
+}
+
+    
