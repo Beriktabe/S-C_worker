d12886a9a1d8be4735a93b2c426959a4995676a2
Beriktabe <Beriktabe@gmail.com>
2025-02-14 11:38:46 +0300

    Add files via upload

---
+++ Client PC/Client PC/Form1.cs
@@ -0,0 +1,20 @@
+﻿using Client_PC.Forms;
+using System;
+using System.Windows.Forms;
+
+namespace Client_PC
+{
+    public partial class Form1 : Form
+    {
+        public static WorkList WorkLst = new WorkList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WorkLst.Show();
+        }
+    }
+}
